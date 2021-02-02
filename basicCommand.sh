1daae91d47d32c6d69f94b3c70c7c132c0f9ff83
Jérémie Perreault <jperreault0211@gmail.com>
2021-02-01 19:15:55 -0500

    ajout pwd

---
+++ basicCommand.sh
@@ -0,0 +1,19 @@
+echo cd : change directory, shared disk : /media/sf_visual_code_share_folder
+
+echo ls : list of directory in the directory you are
+
+echo touch : create a file
+
+echo mkdir : create a directory
+
+echo rm : remove file, -d : remove empty directory, -r remove a non-empty directory
+
+echo rmdir : remove empty directory
+
+echo chmod : permission, exemple : u = rwx read write x = execution u = user/ g = group / o = others, chmod u=rwx g=rx o=r basicCommand.sh
+
+echo sudo : 
+
+echo mv : 
+
+echo pwd : 
